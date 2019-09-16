2d8895000d8b80470a47c6557ba52f26e5a76460
Maria Skrzypek-Markiewicz <markiewiczmaria23@gmail.com>
2019-09-16 10:50:06 +0200

    Update city.js

--- city.js
+++ city.js
@@ -6,12 +6,6 @@ function city() {
             frameRate(30);
 
             // ProgramCodeGoesHere
-
-
-
-
-
-
             var xPos = 317;
             var yPos = 57;
             var starSize = 10;
@@ -20,12 +14,7 @@ function city() {
             var a = 154;
 
             draw = function () {
-
-
                 background(29, 40, 115);
-
-
-
                 fill(212, 222, 29); //znikajaca gwiazda
                 ellipse(50, 50, star2Size, star2Size);
                 ellipse(160, 87, star2Size, star2Size);
@@ -39,14 +28,11 @@ function city() {
 
                 arc(200, 91, 53, 59, -97, 107);
 
-
-
                 fill(99, 82, 82); //wiezowiec 1
                 rect(50, 269, 27, 112);
-
                 fill(224, 179, 32); //okna
 
-                for (var i = 0; i < 5; i++) { //to samo, co niżej ale sprytniej:)
+                for (var i = 0; i < 5; i++) {
                     var starY = 277 + 15 * i;
                     rect(53, starY, 8, 10);
                 }
@@ -55,33 +41,10 @@ function city() {
                     rect(64, starY2, 8, 10);
 
                 }
-                /* rect(53,277, 8, 10);
-                 rect(53,292, 8, 10);
-                 rect(64,277, 8, 10);
-                 rect(64,292, 8, 10);
-                 rect(53,306, 8, 10);
-                 rect(64,306, 8, 10);
-                 rect(64,319, 8, 10);
-                 rect(53,319, 8, 10);
-                 rect(64,332, 8, 10);
-                 rect(53,332, 8, 10);
-                 fill(110, 54, 13);*/
                 rect(61, 350, 13, 28); //okna
-
-
-
 
                 fill(71, 44, 44); //droga
                 rect(0, 379, 400, 30);
-
-                /* fill(255, 0, 0);//samochod poruszany myszką:)
-                 rect(mouseX, 360, 100, 30);
-                 rect(mouseX + 32, 338, 60, 22);
-                 fill(13, 12, 12);
-                 ellipse(mouseX + 16, 386, 25, 25);
-                 ellipse(mouseX + 85, 386, 25, 25);*/
-
-
 
                 fill(255, 0, 0); //samochod
                 rect(carPos, 360, 100, 30);
@@ -99,14 +62,6 @@ function city() {
                     rect(103, rectY, 33, 12);
 
                 }
-                /*rect(103,268,33,12);//okna
-                rect(103,286,33,12);
-                rect(103,306,33,12);
-                rect(103,326,33,12);
-                rect(103,346,33,12);
-                rect(103,366,33,12);
-                rect(103,386,33,12);*/
-
 
                 fill(255, 242, 0); //spadajaca gwiazda
                 ellipse(xPos, yPos, starSize, starSize);
@@ -136,8 +91,8 @@ function city() {
                 frameRate(95); //liczba klatek na sek, czy szybko jedzie czy wolno
                 carPos -= 2;
                 star2Size -= 1;
-                //starSize *= 200/199;//nie wiem jak to zatrzymac, wraca po pewnym czasie jako zolta plama i zajmuje caly ekran...
-                if (carPos === -200) {
+
+                if (carPos === -200) { //zatrzymanie draw
                     noLoop();
 
 
